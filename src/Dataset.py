4f901e130992099fb130990c461ca95aaaeaece6
Justin Merkel <merkel.justin@gmail.com>
2018-01-09 18:52:45 -0600

    revised proposal

--- src/Dataset.py
+++ src/Dataset.py
@@ -4,6 +4,15 @@ import pandas as pd
 import os
 
 def read_data_entry_csv():
-    dataset = pd.read_csv("..{0}res{0}Data_Entry_2017.csv".format(os.path.sep))
+    return pd.read_csv("..{0}res{0}Data_Entry_2017.csv".format(os.path.sep))
 
+dataset = read_data_entry_csv()
 
+value_counts = dataset['Finding Labels'].value_counts()
+
+num_images = len(dataset['Finding Labels'])
+num_healthy_images = value_counts['No Finding']
+
+print("Total number of images: {}".format(num_images))
+print("Total number of healthy images: {}".format(num_healthy_images))
+print("Total number of unhealthy images: {}".format(num_images - num_healthy_images))
