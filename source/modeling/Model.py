7da0ab532e6f5237244f6ef8f140209821e4a3ea
Justin Merkel <merkel.justin@gmail.com>
2018-01-21 17:57:03 -0600

    now processing data in batch

--- source/modeling/Model.py
+++ source/modeling/Model.py
@@ -6,50 +6,168 @@ from keras.models import Sequential, Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler, TensorBoard
 from keras import applications
 from keras import optimizers
+import time
+from keras import backend as K
+from sklearn.metrics import fbeta_score
 
 
-def train_model(total_images=None, epochs=5):
-    train_imgs, train_targets, validation_imgs, validation_targets, test_imgs, test_targets = split_that_shit(size=total_images)
-    img_vectors_train = vectorize_images(train_imgs).astype('float32')/255
-    img_vectors_validation = vectorize_images(validation_imgs).astype('float32')/255
-    img_vectors_test = vectorize_images(test_imgs).astype('float32')/255
+LAYERS_TO_FREEZE = 172
 
-    model = applications.ResNet50(weights="imagenet", include_top=False, input_shape=(512, 512, 3))
 
-    for layer in model.layers:
+def fbeta(y_true, y_pred, beta=2, threshold_shift=0):
+    """Calculate fbeta score for Keras metrics.
+    from https://www.kaggle.com/arsenyinfo/f-beta-score-for-keras
+    """
+    # just in case of hipster activation at the final layer
+    y_pred = K.clip(y_pred, 0, 1)
+
+    # shifting the prediction threshold from .5 if needed
+    y_pred_bin = K.round(y_pred + threshold_shift)
+
+    tp = K.sum(K.round(y_true * y_pred_bin)) + K.epsilon()
+    fp = K.sum(K.round(K.clip(y_pred_bin - y_true, 0, 1)))
+    fn = K.sum(K.round(K.clip(y_true - y_pred, 0, 1)))
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+    beta_squared = beta ** 2
+    fbeta = ((beta_squared + 1) * (precision * recall) /
+             (beta_squared * precision + recall + K.epsilon()))
+    return fbeta
+
+
+def add_new_last_layer(base_model, nb_classes):
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(nb_classes, activation='softmax')(x)
+    model = Model(input=base_model.input, output=predictions)
+    return model
+
+
+def setup_to_transfer_learn(model, base_model):
+    for layer in base_model.layers:
         layer.trainable = False
+    model.compile(
+        optimizer='rmsprop',
+        loss='categorical_crossentropy',
+        metrics=[fbeta]
+    )
+    return model
 
-    # Adding custom Layers
-    x = model.output
-    x = Flatten()(x)
-    x = Dense(1024, activation="relu")(x)
-    # x = Dropout(0.5)(x)
-    x = Dense(1024, activation="relu")(x)
-    predictions = Dense(2, activation="softmax")(x)
 
-    # creating the final model
-    model_final = Model(input=model.input, output=predictions)
+def setup_to_finetune(model):
+    for layer in model.layers[:LAYERS_TO_FREEZE]:
+        layer.trainable = False
+    for layer in model.layers[LAYERS_TO_FREEZE:]:
+        layer.trainable = True
+    model.compile(
+        optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
+        loss='categorical_crossentropy',
+        metrics=[fbeta]
+    )
+    return model
 
-    # compile the model
-    model_final.compile(loss="categorical_crossentropy", optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
-                        metrics=["accuracy"])
 
-    checkpointer = ModelCheckpoint(filepath='..{0}..{0}saved_models{0}resnet50_best.hdf5'.format(os.path.sep),
-                                   verbose=0, save_best_only=True)
+def build_model_from_scratch(output_nodes):
+    model = Sequential()
+    conv1 = Conv2D(filters=16, kernel_size=2, strides=2, padding='same', activation='relu', input_shape=(224, 224, 3))
+    model.add(conv1)
+    pool1 = MaxPooling2D((2, 2), padding='same')
+    model.add(pool1)
+    conv2 = Conv2D(filters=32, kernel_size=4, strides=2, padding='same', activation='relu')
+    model.add(conv2)
+    pool2 = MaxPooling2D((2, 2), padding='same')
+    model.add(pool2)
+    conv3 = Conv2D(filters=64, kernel_size=2, strides=2, padding='same', activation='relu')
+    model.add(conv3)
+    pool3 = MaxPooling2D((2, 2), padding='same')
+    model.add(pool3)
+    gap1 = GlobalAveragePooling2D()
+    model.add(gap1)
+    model.add(Dense(output_nodes, activation="softmax"))
+    return model
 
-    model_final.fit(img_vectors_train, train_targets, validation_data=(img_vectors_validation, validation_targets),
-              epochs=epochs, batch_size=20, callbacks=[checkpointer], verbose=0)
 
-    model_final.load_weights('..{0}..{0}saved_models{0}resnet50_best.hdf5'.format(os.path.sep))
+def build_batch_generator(X, y, batch_size, number_of_batches):
+    counter = 0
+    number_of_batches = np.ceil(X.shape[0] / batch_size)
+    while True:
+        idx_start = batch_size * counter
+        idx_end = batch_size * (counter + 1)
+        if idx_end > len(X):
+            idx_end = len(X)
+        x_batch = vectorize_images(X[idx_start:idx_end]).astype('float32')/255
+        y_batch = np_utils.to_categorical(y[idx_start:idx_end], 2)
+        counter += 1
+        yield x_batch, y_batch
+        if counter == number_of_batches-1:
+            counter = 0
+
+
+def train_model(total_images=None, batch_size=32, epochs=5):
+    print("Starting image vectorization process...")
+    start_time = time.time()
+    train_X, train_y, validation_X, validation_y, test_X, test_y = split_that_shit(size=total_images)
+
+    steps_per_epoch_fit = np.ceil(len(train_X) / batch_size)
+    steps_per_epoch_val = np.ceil(len(validation_X) / batch_size)
+
+    training_generator = build_batch_generator(train_X, train_y, batch_size, steps_per_epoch_fit)
+    validation_generator = build_batch_generator(validation_X, validation_y, batch_size, steps_per_epoch_val)
+
+    print("Done. Total time: {} sec".format(time.time() - start_time))
+
+    print("Beginning fitting network...")
+    start_time = time.time()
+    checkpointer = ModelCheckpoint(
+        filepath='..{0}..{0}saved_models{0}resnet50_best.hdf5'.format(os.path.sep),
+        verbose=0,
+        save_best_only=True
+    )
+    base_model = applications.InceptionV3(weights='imagenet', include_top=False)
+    model = add_new_last_layer(base_model, 2)
+
+    # transfer learning
+    model = setup_to_transfer_learn(model, base_model)
+
+    model.fit_generator(
+        generator=training_generator,
+        steps_per_epoch=steps_per_epoch_fit,
+        validation_data=validation_generator,
+        validation_steps=steps_per_epoch_val,
+        epochs=epochs,
+        verbose=1,
+        callbacks=[checkpointer]
+    )
+
+    # fine-tuning
+    model = setup_to_finetune(model)
+
+    model.fit_generator(
+        generator=training_generator,
+        steps_per_epoch=steps_per_epoch_fit,
+        validation_data=validation_generator,
+        validation_steps=steps_per_epoch_val,
+        epochs=epochs,
+        verbose=1,
+        callbacks=[checkpointer]
+    )
+
+    model.load_weights('..{0}..{0}saved_models{0}resnet50_best.hdf5'.format(os.path.sep))
+    print("Done. Total time: {} sec".format(time.time() - start_time))
 
     # get index of predicted dog breed for each image in test set
-    model_predictions = [np.argmax(model_final.predict(np.expand_dims(img, axis=0))) for img in img_vectors_test]
+    model_predictions = [np.argmax(model.predict(
+        np.expand_dims(vectorize_img(img), axis=0))
+    ) for img in test_X]
 
     # report test accuracy
-    test_accuracy = 100 * np.sum(np.array(model_predictions) == np.argmax(test_targets, axis=1)) / len(test_targets)
+    test_accuracy = 100 * np.sum(np.array(model_predictions) == np.argmax(test_y, axis=1)) / len(test_y)
     print('Test accuracy: %.4f%%' % test_accuracy)
     print("Training Done!")
 
 
 if __name__ == '__main__':
-    train_model(total_images=1000, epochs=5)
+    train_model(epochs=5, total_images=100)
--- source/preprocessing/Dataset.py
+++ source/preprocessing/Dataset.py
@@ -7,10 +7,29 @@ from keras.preprocessing import image
 import random
 from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 RES_PATH = "..{0}..{0}resources{0}".format(os.path.sep)
 IMG_PATH = "..{0}..{0}images{0}".format(os.path.sep)
+
+
+def move_images():
+    with open("{}{}".format(RES_PATH, "test_list.txt"), 'r') as f:
+        for file_name in f:
+            file_name = file_name[:-1]
+            os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}".format(IMG_PATH, "test", os.path.sep, file_name))
+
+    with open("{}{}".format(RES_PATH, "train_val_list.txt"), 'r') as f:
+        for file_name in f:
+            file_name = file_name.replace('\n', '')
+            os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}".format(IMG_PATH, "train", os.path.sep, file_name))
+
+
+def chunk_list(images, chuncks):
+    random.shuffle(images)
+    size = len(images) / chuncks
+    return [images[i:i+size] for i in range(0, len(images), size)]
 
 
 def read_data_entry_csv():
@@ -18,10 +37,10 @@ def read_data_entry_csv():
 
 
 def read_data_entry_preprocessed_csv(size=None):
-    if not size:
-        return pd.read_csv("{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH))
-    else:
-        return pd.read_csv("{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH))[:size]
+    df = pd.read_csv("{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH))
+    if size:
+        df = df[:size]
+    return df
 
 
 def reclassify_findings_column(row):
@@ -29,14 +48,15 @@ def reclassify_findings_column(row):
 
 
 def vectorize_img(img_path):
-    img = image.load_img(img_path, target_size=(512, 512))
+    img = image.load_img(img_path, target_size=(224, 224))
     img_vector = image.img_to_array(img)
     return np.expand_dims(img_vector, axis=0)
 
 
 def vectorize_images(file_names):
-    file_paths = ["{}{}".format(IMG_PATH, i) for i in file_names]
-    list_of_vectors = [vectorize_img(img_path) for img_path in file_paths]
+    if len(file_names) == 0:
+        return np.ndarray([])
+    list_of_vectors = [vectorize_img("{}{}".format(IMG_PATH, i)) for i in file_names]
     return np.vstack(list_of_vectors)
 
 
@@ -53,7 +73,7 @@ def split_that_shit(size=None, test_percentage=0.2, validation_percentage=0.1):
     y = dataset.iloc[:, 1].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_percentage)
     X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_percentage)
-    return X_train.flatten(), format_dependent(y_train), X_validation.flatten(), format_dependent(y_validation), X_test.flatten(), format_dependent(y_test)
+    return X_train.flatten(), y_train, X_validation.flatten(), y_validation, X_test.flatten(), y_test
 
 
 def format_dependent(y, categories=2):
