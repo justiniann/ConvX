065ffbd90156511099546dcf5b0676158b833bce
Justin Merkel <merkel.justin@gmail.com>
2018-03-13 22:05:22 -0500

    completed analysis and updated README

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# ConvX
-Building convolutional neural networks for the analysis of x-ray images.
+# Introduction
+Building convolutional neural networks for the analysis of x-ray images. This analysis is based entirely on a dataset of x-ray images provided by the NIH. For more information on the dataset, visit https://www.nih.gov/news-events/news-releases/nih-clinical-center-provides-one-largest-publicly-available-chest-x-ray-datasets-scientific-community.
 
 # Getting Started
 The dataset used for this project has been provided by the U.S. National Institute of Health. All data used for this project can be downloaded from the at https://nihcc.app.box.com/v/ChestXray-NIHCC.
-In order for this project to work properly, these images need to be downloaded and extracted into the img directory of this project.
-Additionally, the Data_Entry_2017.csv and BBox_List_2017.csv file should be downloaded and added to the res directory of this project.<br /><br />
+In order for this project to work properly, these images need to be downloaded and extracted into the images directory of this project. Additionally, the Data_Entry_2017.csv file should be downloaded and added to the resources directory of this project.
 
-For more information on the dataset, visit https://www.nih.gov/news-events/news-releases/nih-clinical-center-provides-one-largest-publicly-available-chest-x-ray-datasets-scientific-community.
+Once this has been done, simply run the convx_utils.py file in the sources directory. This will preprocess the data from the data entry csv and restructure the images into the following directory structure...
+
+<pre>
+images
+|-----train
+|        |-----healthy
+|        +-----unhealthy
+|-----test
+|        |-----healthy
+|        +-----unhealthy
+|-----validation
+|        |-----healthy
+|        +-----unhealthy
+</pre>
+
+# Running the Code
+Once this is done, the analysis code can be run. You can find this code, along with helpful descriptions, in the convx.ipynb file located in the source directory.
+
--- source/convx/convx_utils.py
+++ source/convx/convx_utils.py
@@ -21,68 +21,8 @@ def read_data_entry_csv():
     return pd.read_csv("{}Data_Entry_2017.csv".format(RES_PATH))
 
 
-def read_data_entry_preprocessed_csv(size=None):
-    df = pd.read_csv("{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH))
-    if size:
-        df = df[:size]
-    return df
-
-
-def split_image_groups(size=None, test_percentage=0.2, validation_percentage=0.1):
-    dataset = read_data_entry_preprocessed_csv(size=size)
-    X = dataset.iloc[:, :-1].values
-    y = dataset.iloc[:, 1].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_percentage)
-    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_percentage)
-    return X_train.flatten(), format_dependent(y_train), X_validation.flatten(), format_dependent(y_validation), X_test.flatten(), format_dependent(y_test)
-
-
-def format_dependent(y, categories=2):
-    return np_utils.to_categorical(y, categories)
-
-
-def ouptut_distribution():
-    dataset = read_data_entry_csv()
-    value_counts = dataset['Finding Labels'].value_counts()
-    num_images = len(dataset['Finding Labels'])
-    num_healthy_images = value_counts['No Finding']
-    print("Total number of images: {}".format(num_images))
-    print("Total number of healthy images: {}".format(num_healthy_images))
-    print("Total number of unhealthy images: {}".format(num_images - num_healthy_images))
-
-
-def move_images():
-    train_X, train_y, validation_X, validation_y, test_X, test_y = split_image_groups()
-    # train
-    for i in range(0, len(train_X)):
-        file_name = train_X[i]
-        classification_directory = "healthy" if np.argmax(train_y[i]) == 0 else "unhealthy"
-        os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}"
-                  .format(TRAIN_PATH, classification_directory, os.path.sep, file_name))
-
-    # validation
-    for i in range(0, len(validation_X)):
-        file_name = validation_X[i]
-        classification_directory = "healthy" if np.argmax(validation_y[i]) == 0 else "unhealthy"
-        os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}"
-                  .format(VAL_PATH, classification_directory, os.path.sep, file_name))
-
-    # test
-    for i in range(0, len(test_X)):
-        file_name = test_X[i]
-        classification_directory = "healthy" if np.argmax(test_y[i]) == 0 else "unhealthy"
-        os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}"
-                  .format(TEST_PATH, classification_directory, os.path.sep, file_name))
-
-
-def preprocess():
-    preprocessed_csv_file = "{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH)
-    if not os.path.exists(preprocessed_csv_file):
-        dataset = read_data_entry_csv()
-        dataset["Health Flag"] = dataset.apply(lambda row: 0 if row['Finding Labels'] == "No Finding" else 1, axis=1)
-        preprocessed_dataset = dataset[['Image Index', 'Health Flag']].copy()
-        preprocessed_dataset.to_csv(preprocessed_csv_file, sep=',', index=False)
-    print("Data successfully preprocessed!")
+def read_data_entry_preprocessed_csv():
+    return pd.read_csv("{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH))
 
 
 def build_dir_path(path):
@@ -98,18 +38,63 @@ def get_iterations_per_epoch(total_images, batch_size):
     return np.ceil(total_images / batch_size)
 
 
-def save_model(model_name, model, save_path):
-    # serialize model to JSON
-    model_json = model.to_json()
-    build_dir_path(save_path)
-    with open(os.path.join(save_path, "best_model.json".format(model_name)), "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights(os.path.join(save_path, "best_weights.h5".format(model_name)))
-    print("Saved model to disk")
+def format_dependent(y, categories=2):
+    return np_utils.to_categorical(y, categories)
+
+
+def split_image_groups(test_percentage=0.2, validation_percentage=0.1):
+    dataset = read_data_entry_preprocessed_csv()
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 1].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_percentage)
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_percentage)
+    return X_train.flatten(), y_train, X_validation.flatten(), y_validation, X_test.flatten(), y_test
+
+
+def preprocess():
+    preprocessed_csv_file = "{}Preprocessed_Data_Entry_2017.csv".format(RES_PATH)
+    if not os.path.exists(preprocessed_csv_file):
+        dataset = read_data_entry_csv()
+        dataset["Health Flag"] = dataset.apply(lambda row: 0 if row['Finding Labels'] == "No Finding" else 1, axis=1)
+        preprocessed_dataset = dataset[['Image Index', 'Health Flag']].copy()
+        preprocessed_dataset.to_csv(preprocessed_csv_file, sep=',', index=False)
+    print("Data successfully preprocessed!")
+
+
+def move_images():
+    train_X, train_y, validation_X, validation_y, test_X, test_y = split_image_groups()
+    # train
+    for i in range(0, len(train_X)):
+        file_name = train_X[i]
+        classification_directory = "healthy" if train_y[i] == 0 else "unhealthy"
+        os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}"
+                  .format(TRAIN_PATH, classification_directory, os.path.sep, file_name))
+
+    # validation
+    for i in range(0, len(validation_X)):
+        file_name = validation_X[i]
+        classification_directory = "healthy" if validation_y[i] == 0 else "unhealthy"
+        os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}"
+                  .format(VAL_PATH, classification_directory, os.path.sep, file_name))
+
+    # test
+    for i in range(0, len(test_X)):
+        file_name = test_X[i]
+        classification_directory = "healthy" if test_y[i] == 0 else "unhealthy"
+        os.rename("{}{}".format(IMG_PATH, file_name), "{}{}{}{}"
+                  .format(TEST_PATH, classification_directory, os.path.sep, file_name))
+
+
+def ouptut_distribution():
+    dataset = read_data_entry_csv()
+    value_counts = dataset['Finding Labels'].value_counts()
+    num_images = len(dataset['Finding Labels'])
+    num_healthy_images = value_counts['No Finding']
+    print("Total number of images: {}".format(num_images))
+    print("Total number of healthy images: {}".format(num_healthy_images))
+    print("Total number of unhealthy images: {}".format(num_images - num_healthy_images))
 
 
 if __name__ == '__main__':
     preprocess()
     move_images()
-
